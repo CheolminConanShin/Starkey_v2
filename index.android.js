c2e9e3f9fc6dc0acd9e4c12d5a82bb42b1bc13d5
CheolminConanShin <scm0226@gmail.com>
2017-03-21 08:19:06 +0900

    initial commit

---
+++ index.android.js
@@ -0,0 +1,4 @@
+import {AppRegistry} from 'react-native';
+import {App} from './App/View/App';
+
+AppRegistry.registerComponent('Starkey_v2', () => App);
---
+++ web/webpack.config.dev.js
@@ -0,0 +1,35 @@
+const path = require('path')
+const webpack = require('webpack')
+
+module.exports = {
+  devServer: {
+    contentBase: path.join(__dirname, 'src')
+  },
+  entry: [
+    path.join(__dirname, '../index.web.js')
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: { cacheDirectory: true }
+      },
+      {
+        test: /\.(gif|jpe?g|png|svg)$/,
+        loader: 'url-loader',
+        query: { name: '[name].[hash:16].[ext]' }
+      }
+    ]
+  },
+  output: {
+    filename: 'bundle.js'
+  },
+  resolve: {
+    alias: {
+      'react-native': 'react-native-web'
+    }
+  }
+}
+
